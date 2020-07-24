daf90cfb6c8243db287238c6ddc0801d5fb76cc2
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-24 16:44:00 +0800

    [Parking lot][Eric] test: should_return_1_ticket_when_parking_boy_parking_car_given_parking_boy_1_car

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+    public ParkingTicket parking(Car car) {
+        return null;
+    }
+}
--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,29 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingTicket;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingBoyFacts {
     @Test
     void write_your_first_test() {
 
+    }
+
+    @Test
+    void should_return_1_ticket_when_parking_boy_parking_car_given_parking_boy_1_car() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy();
+
+        //when
+        ParkingTicket parkingTicket = parkingBoy.parking(car);
+
+        //then
+        assertNotNull(parkingTicket);
     }
 }
