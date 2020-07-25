124201ba847be02cb974c3c0c850a6893f7f43fc
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-25 17:31:11 +0800

    [Parking lot][Eric] featrue: pass return_parking_lots_has_1_cars_and_1_car_when_check_parking_order_parking_lots_has_1_cars_and_0_cars_1_car_1_smart_parking_boy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingBoy {
-    private List<ParkingLot> parkingLots;
+    protected List<ParkingLot> parkingLots;
 
     public ParkingBoy(int parkingLotsCount) {
         parkingLots = new ArrayList<>();
--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -7,4 +7,20 @@ public class SmartParkingBoy extends ParkingBoy {
     public SmartParkingBoy(int parkingLotsCount) {
         super(parkingLotsCount);
     }
+
+    @Override
+    public ParkingTicket parkingCar(Car car) {
+        ParkingTicket parkingTicket = new ParkingTicket(car.getCarId());
+        ParkingLot maxPositionParkingLot = new ParkingLot();
+        int bestParkingLotNumber = 0;
+        int maxPosition = parkingLots.get(bestParkingLotNumber).getPosition();
+        for (int parkingLotNumber = 0; parkingLotNumber < parkingLots.size(); parkingLotNumber++) {
+            if (parkingLots.get(parkingLotNumber).getPosition() > maxPosition) {
+                bestParkingLotNumber = parkingLotNumber;
+                maxPosition = parkingLots.get(parkingLotNumber).getPosition();
+            }
+        }
+        parkingLots.get(bestParkingLotNumber).parkingCarToParkingLot(car);
+        return parkingTicket;
+    }
 }
